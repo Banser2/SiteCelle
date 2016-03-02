edf8d412a60975b20273f285ac0a1c0150590177
DulciSantos <santosdulcineide1@gmail.com>
2016-03-02 14:08:44 -0300

    Pagina para exibir o resultado final do quiz. Dupla: Dulcineide e Gleibson

---
+++ principal/php/resultado_final.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<script src="jquery-1.12.0.min.js"></script>
+	<style type="text/css">
+	.rs{
+		border:;
+		text-align: center;
+
+
+	}
+	</style>
+	</head>
+	<body>
+
+		<div class="rs">
+
+</div>
+</body>
+</html>
