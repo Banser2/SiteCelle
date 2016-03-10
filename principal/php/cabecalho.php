6bb8b9522075c1929c7ccd0a6394007faa8c8d63
bertonnipaz <bertonnipaz@gmail.com>
2016-03-10 13:21:34 -0300

    modificando template para php

---
+++ principal/php/cabecalho.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<title>Sobre</title>
+		<meta charset="utf-8">
+		<script src="../javaScript/jquery-1.12.0.min.js"></script>
+		<link rel="shortcut icon" href=",,/imagens/logoIFPE.png"/>
+        <link rel="stylesheet" type="text/css" href="../css/estilo.css" media="all">
+		<link rel="stylesheet" type="text/css" href="../css/estilo_quiz.css" media="all">
+		<script src="../javaScript/javaScript.js"></script>
+	</head>
+	<body>
+       <header>
+        <div id="logo">
+                <div class="banner">
+                    <div id="logo-reitoria">
+                        <img src="../imagens/Logo.png" widht="300px" height="90px">
+                        <a class="acesso-reitoria"></a>
+                    </div>
+                </div>
+            </div>  
+          <nav id="menu">
+        <ul class="menu">        
+        <li><a href="home.php" class="botaoMenu" id="home.php">Home</a></li>
+        <li><a href="sobre.php" class="botaoMenu" id="sobre.php">Sobre</a></li>
+        <li><a href="noticias.php" class="botaoMenu" id="noticias.php">Notícias</a></li>
+        <li><a href="#" id="#">Mídia</a>
+            <ul>
+                <li><a href="videos.php" class="botaoMenu" id="videos.php">Vídeos</a></li>
+                <li><a href="audios.php" class="botaoMenu" id="audios.php">Áudios</a></li>
+            </ul>
+        </li>
+        <li><a href="#" id="#">Atividades</a>
+            <ul>
+                <li><a href="quiz.php" class="botaoMenu" id="quiz.php">Quiz</a></li>
+            </ul>
+        </li>
+        <li><a href="sites_relacionados.php" class="botaoMenu" id="sites_relacionados.php">Sites Relacionados</a></li>
+        <li><a href="contatos.php" class="botaoMenu" id="CONTATOS.php">Contato</a></li>
+</ul>
+<br><br>
+ </nav>
+</header> 
