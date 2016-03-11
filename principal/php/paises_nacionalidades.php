9f0fe1be6993ae8d112dfca8d23dd285468615b5
gleibsony <gleibsonytalo12@gmail.com>
2016-03-11 13:01:44 -0300

    modificando paginas paises_nacionalidades, e o estilo

--- principal/php/paises_nacionalidades.php
+++ principal/php/paises_nacionalidades.php
@@ -6,126 +6,140 @@
 	<meta charset="utf-8">
 	<title>Tabela</title>
 </head>
-<style>
-table,tr,td,th{
-border: 2px solid;
-border-collapse: collapse;
-}
-th{
-text-align: left;
-}
+<style type="text/css">
+    * {
+        font-family:sans-serif;
+    }
+    table, tr, td, th{
+        border-collapse: collapse;
+        padding: 8px 10px;
+    }
+    table tr: first-child{
+        background-color: #353535;
+        color: #FFF
+    }
+    table tr:nth-child(even){
+        background-color: #DFDFDF;
+    }
+    th{
+        text-align: left;
+    }
 
 </style>
 <body>
-<center><table></center>
-  <h2>Paises e Nacionalidades</h2>		
-    <tr>
-    	<th>Português</th>
-    	<th>Inglês</th>
-    	<th>Nacionalidades</th>
-    </tr>
-            <tr>
-                <td>Afeganistão</td>
-                <td>Afghanistan</td>
-                <td>Afgban</td>
-            </tr>
-            <tr>
-                <td>África do Sul</td>
-                <td>South Africa</td>
-                <td>South African</td>
-            </tr>
-            <tr>
-                <td>Alemanha</td>
-                <td>Germany</td>
-                <td>German</td>
-            </tr>	
-            <tr>
-                <td>Arábia Saudita</td>
-                <td>Saudi Arabia</td>
-                <td>Saudi</td>
-            </tr>	
-            <tr>
-                <td>Argentina</td>
-                <td>Argentina</td>
-                <td>Argentinian</td>
-            </tr>	
-            <tr>
-                <td>Austrália</td>
-                <td>Australia</td>
-                <td>Australian</td>
-            </tr>	
-            <tr>
-                <td>Áustria</td>
-                <td>Austria</td>
-                <td>Austrian</td>
-            </tr>	
-            <tr>
-                <td>Bélgica</td>
-                <td>Belgium</td>
-                <td>Belgian</td>
-            </tr>	
-            <tr>
-                <td>Bolívia</td>
-                <td>Bolivia</td>
-                <td>Bolivian</td>
-            </tr>	
-            <tr>
-                <td>Brasil</td>
-                <td>Brazil</td>
-                <td>Brazilian</td>
-            </tr>				
-            <tr>
-                <td>Bulgária</td>
-                <td>Bulgaria</td>
-                <td>Bulgarian</td>
-            </tr>	
-            <tr>
-                <td>Canadá</td>
-                <td>Canada</td>
-                <td>Canadian</td>
-            </tr>	
-            <tr>
-                <td>Chile</td>
-                <td>Chile</td>
-                <td>Chilean</td>
-            </tr>	
-            <tr>
-                <td>China</td>
-                <td>China</td>
-                <td>Chinese</td>
-            </tr>	
-            <tr>
-                <td>Colômbia</td>
-                <td>Colombia</td>
-                <td>Colombian</td>
-            </tr>	
-            <tr>
-                <td>Coréia</td>
-                <td>Korea</td>
-                <td>Korean</td>
-            </tr>	
-            <tr>
-                <td>Cuba</td>
-                <td>Cuba</td>
-                <td>Cuban</td>
-            </tr>	
-            <tr>
-                <td>Dinamarca</td>
-                <td>Denmark</td>
-                <td>Dane</td>
-            </tr>	
-            <tr>
-                <td>Egito</td>
-                <td>Egypt</td>
-                <td>Egyptian</td>
-            </tr>	
-            <tr>
+    <section>
+        <div class="conteudo">
+            <center><table></center>
+                <h2>Paises e Nacionalidades</h2>		
+                        <tr>
+                            <th>Português</th>
+                            <th>Inglês</th>
+                            <th>Nacionalidades</th>
+                        </tr>
+                        <tr>
+                            <td>Afeganistão</td>
+                            <td>Afghanistan</td>
+                            <td>Afgban</td>
+                        </tr>
+                        <tr>
+                            <td>África do Sul</td>
+                            <td>South Africa</td>
+                            <td>South African</td>
+                        </tr>
+                        <tr>
+                            <td>Alemanha</td>
+                            <td>Germany</td>
+                            <td>German</td>
+                        </tr>	
+                        <tr>
+                            <td>Arábia Saudita</td>
+                            <td>Saudi Arabia</td>
+                            <td>Saudi</td>
+                        </tr>	
+                        <tr>
+                            <td>Argentina</td>
+                            <td>Argentina</td>
+                            <td>Argentinian</td>
+                        </tr>	
+                        <tr>
+                            <td>Austrália</td>
+                            <td>Australia</td>
+                            <td>Australian</td>
+                        </tr>	
+                        <tr>
+                            <td>Áustria</td>
+                            <td>Austria</td>
+                            <td>Austrian</td>
+                        </tr>	
+                        <tr>
+                            <td>Bélgica</td>
+                            <td>Belgium</td>
+                            <td>Belgian</td>
+                        </tr>	
+                        <tr>
+                            <td>Bolívia</td>
+                            <td>Bolivia</td>
+                            <td>Bolivian</td>
+                        </tr>	
+                        <tr>
+                            <td>Brasil</td>
+                            <td>Brazil</td>
+                            <td>Brazilian</td>
+                        </tr>				
+                        <tr>
+                            <td>Bulgária</td>
+                            <td>Bulgaria</td>
+                            <td>Bulgarian</td>
+                        </tr>	
+                        <tr>
+                            <td>Canadá</td>
+                            <td>Canada</td>
+                            <td>Canadian</td>
+                        </tr>	
+                        <tr>
+                            <td>Chile</td>
+                            <td>Chile</td>
+                            <td>Chilean</td>
+                        </tr>	
+                        <tr>
+                            <td>China</td>
+                            <td>China</td>
+                            <td>Chinese</td>
+                        </tr>	
+                        <tr>
+                            <td>Colômbia</td>
+                            <td>Colombia</td>
+                            <td>Colombian</td>
+                        </tr>	
+                        <tr>
+                            <td>Coréia</td>
+                            <td>Korea</td>
+                            <td>Korean</td>
+                        </tr>	
+                        <tr>
+                            <td>Cuba</td>
+                            <td>Cuba</td>
+                            <td>Cuban</td>
+                        </tr>	
+                        <tr>
+                            <td>Dinamarca</td>
+                            <td>Denmark</td>
+                            <td>Dane</td>
+                        </tr>	
+                        <tr>
+                            <td>Egito</td>
+                            <td>Egypt</td>
+                            <td>Egyptian</td>
+                        </tr>	
+                        <tr>
                 <td>Equador</td>
                 <td>Ecuador</td>
                 <td>Ecuadorian</td>
-            </tr>
-</table>
-<a href="paises_nacionalidades1.php">Proximo</a>
+                        </tr>
+        </table>
+        <a href="paises_nacionalidades1.php">Proximo</a>
+        </div>
+    </section>
 </body>
 </html>
 <?php 
--- principal/php/paises_nacionalidades2.php
+++ principal/php/paises_nacionalidades2.php
@@ -6,111 +6,126 @@
 	<meta charset="utf-8">
 	<title>Tabela</title>
 </head>
-<style>
-table,tr,td,th{
-border: 2px solid;
-border-collapse: collapse;
-}
-th{
-text-align: left;
-}
+<style type="text/css">
+    * {
+        font-family:sans-serif;
+    }
+    table, tr, td, th{
+        border-collapse: collapse;
+        padding: 8px 10px;
+    }
+    table tr: first-child{
+        background-color: #353535;
+        color: #FFF
+    }
+    table tr:nth-child(even){
+        background-color: #DFDFDF;
+    }
+    th{
+        text-align: left;
+    }
 
 </style>
 <body>
-<center><table></center>
-            <tr>
-                <td>Portugal</td>
-                <td>Portugal</td>
-                <td>Portuguese</td>
-            </tr>
-            <tr>
-                <td>Mônaco</td>
-                <td>Monaco</td>
-                <td>Monacan</td>
-            </tr>
-            <tr>
-                <td>Noruega</td>
-                <td>Norway</td>
-                <td>Norwegian</td>
-            </tr>
-            <tr>
-                <td>Nova Zelâdia</td>
-                <td>New Zeland</td>
-                <td>New Zeland</td>
-            </tr>
-            <tr>
-                <td>Panamá</td>
-                <td>Panama</td>
-                <td>Panamanian</td>
-            </tr>
-            <tr>
-                <td>Paquistão</td>
-                <td>Pakistan</td>
-                <td>Pakistani</td>
-            </tr>
-            <tr>
-                <td>Paraguai</td>
-                <td>Paraguay</td>
-                <td>Paraguayan</td>
-            </tr>
-            <tr>
-                <td>Peru</td>
-                <td>Peru</td>
-                <td>Peruvian</td>
-            </tr>
-            <tr>
-                <td>Polônia</td>
-                <td>Poland</td>
-                <td>Pole</td>
-            </tr>
-            <tr>
-                <td>Portugal</td>
-                <td>Portugal</td>
-                <td>Portuguese</td>
-            </tr>
-            <tr>
-                <td>Rússia</td>
-                <td>Russia</td>
-                <td>Russian</td>
-            </tr>
-            <tr>
-                <td>Suécia</td>
-                <td>Sweden</td>
-                <td>Swede</td>
-            </tr>
-            <tr>
-                <td>Suíça</td>
-                <td>Swizerland</td>
-                <td>Swiss</td>
-            </tr>
-            <tr>
-                <td>Tailândia</td>
-                <td>Thailand</td>
-                <td>Thai</td>
-            </tr>
-            <tr>
-                <td>Turquia</td>
-                <td>Turkey</td>
-                <td>Turkish</td>
-            </tr>
-            <tr>
-                <td>Venezuela</td>
-                <td>Venezuela</td>
-                <td>Venezuelan</td>
-            </tr>
-            <tr>
-                <td>Uruguai</td>
-                <td>Uruguay</td>
-                <td>Uruguayan</td>
-            </tr>
-            <tr>
-                <td>Vietnã</td>
-                <td>Vietnam</td>
-                <td>Vietnamese</td>
-            </tr>
+    <section>
+        <div class="conteudo">
+            <center><table></center>
+                <h2>Paises e Nacionalidades</h2>    
+                    <tr>
+                        <td>Portugal</td>
+                        <td>Portugal</td>
+                        <td>Portuguese</td>
+                    </tr>
+                    <tr>
+                        <td>Mônaco</td>
+                        <td>Monaco</td>
+                        <td>Monacan</td>
+                    </tr>
+                    <tr>
+                        <td>Noruega</td>
+                        <td>Norway</td>
+                        <td>Norwegian</td>
+                    </tr>
+                    <tr>
+                        <td>Nova Zelâdia</td>
+                        <td>New Zeland</td>
+                        <td>New Zeland</td>
+                    </tr>
+                    <tr>
+                        <td>Panamá</td>
+                        <td>Panama</td>
+                        <td>Panamanian</td>
+                    </tr>
+                    <tr>
+                        <td>Paquistão</td>
+                        <td>Pakistan</td>
+                        <td>Pakistani</td>
+                    </tr>
+                    <tr>
+                        <td>Paraguai</td>
+                        <td>Paraguay</td>
+                        <td>Paraguayan</td>
+                    </tr>
+                    <tr>
+                        <td>Peru</td>
+                        <td>Peru</td>
+                        <td>Peruvian</td>
+                    </tr>
+                    <tr>
+                        <td>Polônia</td>
+                        <td>Poland</td>
+                        <td>Pole</td>
+                    </tr>
+                    <tr>
+                        <td>Portugal</td>
+                        <td>Portugal</td>
+                        <td>Portuguese</td>
+                    </tr>
+                    <tr>
+                        <td>Rússia</td>
+                        <td>Russia</td>
+                        <td>Russian</td>
+                    </tr>
+                    <tr>
+                        <td>Suécia</td>
+                        <td>Sweden</td>
+                        <td>Swede</td>
+                    </tr>
+                    <tr>
+                        <td>Suíça</td>
+                        <td>Swizerland</td>
+                        <td>Swiss</td>
+                    </tr>
+                    <tr>
+                        <td>Tailândia</td>
+                        <td>Thailand</td>
+                        <td>Thai</td>
+                    </tr>
+                    <tr>
+                        <td>Turquia</td>
+                        <td>Turkey</td>
+                        <td>Turkish</td>
+                    </tr>
+                    <tr>
+                        <td>Venezuela</td>
+                        <td>Venezuela</td>
+                        <td>Venezuelan</td>
+                    </tr>
+                    <tr>
+                        <td>Uruguai</td>
+                        <td>Uruguay</td>
+                        <td>Uruguayan</td>
+                    </tr>
+                    <tr>
+                        <td>Vietnã</td>
+                        <td>Vietnam</td>
+                        <td>Vietnamese</td>
+                    </tr>
 </table>
 <a href="paises_nacionalidades.php">Inicio</a>
 <a href="paises_nacionalidades1.php">Anterior</a>
+        </div>
+    </section>
 </body>
 </html>
 <?php 
