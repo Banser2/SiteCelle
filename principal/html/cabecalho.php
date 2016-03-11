49f7e10a427b147b44c6ecc07e26ecdbcb549211
brunomds <bruno.net12@hotmail.com>
2016-03-11 08:55:09 -0300

    mudança das paginas para php e acrescentamos a pagina de cabeçalho e rodape

---
+++ principal/html/cabecalho.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<title>Sobre</title>
+		<meta charset="utf-8">
+		<script src="javaScript/jquery-1.12.0.min.js"></script>
+		<link rel="shortcut icon" href="Imagens/logoIFPE.png"/>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css" media="all">
+		<script src="javaScript/javaScript.js"></script>
+	</head>
+	<body>
+       <header>
+        <div id="logo">
+                <div class="banner">
+                    <div id="logo-reitoria">
+                        <img src="Imagens/Logo.png" widht="300px" height="90px">
+                        <a class="acesso-reitoria"></a>
+                    </div>
+                </div>
+            </div>  
+          <nav id="menu">
+        <ul class="menu">        
+        <li><a href="#" class="botaoMenu" id="home.html">Home</a></li>
+        <li><a href="#" class="botaoMenu" id="sobre.html">Sobre</a></li>
+        <li><a href="#" class="botaoMenu" id="noticias.html">Notícias</a></li>
+        <li><a href="#" id="#">Mídia</a>
+            <ul>
+                <li><a href="#" class="botaoMenu" id="videos.html">Vídeos</a></li>
+                <li><a href="#" class="botaoMenu" id="audios.html">Áudios</a></li>
+            </ul>
+        </li>
+        <li><a href="#" class="botaoMenu" id="sites_relacionados.html">Sites Relacionados</a></li>
+        <li><a href="#" class="botaoMenu" id="contatos.html">Contato</a></li>
+        <li><a href="#" class="botaoMenu" id="exercicio.php">exercicio</a><li>
+            </ul>
+<br><br><br>
+ </nav>
+</header>
---
+++ principal/html/rodape.php
@@ -0,0 +1,22 @@
+<footer>
+<br>
+Avenida Alfredo Bandeira de Melo, BR 101 norte - km 44
+Centro, Igarassu - PE, Cep: 53610-000<br>
+Fale com o CELLE:
+celle@igarassu.ifpe.edu.br
+ (81) 3334 - 3507    
+</footer>
+		<script>
+		$( window ).load(function() {
+		  $('#conteudo').load('html/sobre.html');
+		});
+			
+        $('.botaoMenu').click(function(event) {
+			var pagina = event.target.id;
+            $('#conteudo').load('html/' +pagina);
+        });
+		
+
+      </script>
+    </body>
+    </html>
