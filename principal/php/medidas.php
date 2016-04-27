7ea825ef2f70b58721ef995062b2e180ee0a3b91
Gleibsony <gleibsonytalo12@gmail.com>
2016-04-27 11:08:10 -0300

    adicionando a pagina medidas adicionando a pagina numero adicionando a pagina arquivos esta pagina arquivos é onde iremos adicionar as atividades feitas pelo aluno

---
+++ principal/php/medidas.php
@@ -0,0 +1,69 @@
+<?php 
+require 'cabecalho.php';   
+?>
+<html>
+	<meta charset="UTF-8">
+	<div id="conteudo">
+        <h1 class="t1">Medidas</h1>
+            <hr id="hr-top">
+        <div class="paises">
+        <table border="1">
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>1 Inch</td> 
+                <td>  </td>
+                <td>2,54 cm</td> 
+            </tr>
+            <tr>
+                <td>1 Foor</td> 
+                <td>12 Inches</td> 
+                <td>30,48 cm</td> 
+            </tr> 
+            <tr>
+                <td>1 yard</td> 
+                <td>3 feet</td> 
+                <td>91,44 cm</td> 
+            </tr>  
+            <tr>
+                <td>1 Pole, Rod, Perch</td> 
+                <td>5 1/2</td> 
+                <td>5,03 m</td> 
+            </tr> 
+            <tr>
+                <td>1 Chain(Ingl.)</td> 
+                <td>4 poles, Rod, Perch</td> 
+                <td>20,12 m</td> 
+            </tr>
+            <tr>
+                <td>1 Furlong (200 yards)</td>
+                <td>10 Chains (40 rods)</td> 
+                <td>201,17 m</td> 
+            </tr>
+            <tr>
+                <td>1 Statute mile (1760 yards)</td> 
+                <td>8 furlongs (5280 feet)</td> 
+                <td>1.609 km</td> 
+            </tr>
+            <tr>
+                <td>1 nautical mile</td> 
+                <td>6080.2 feet</td> 
+                <td>1.853 km</td> 
+            </tr>
+            <tr>
+                <td>1 league</td> 
+                <td>3 statute miles</td> 
+                <td>4.828 km</td> 
+            </tr>
+            </table>
+            <span><small>Page 1 of 1</small></span>
+            <a class="next_prev" href="medidas2.php">Próximo</a>
+        </div>
+    </div>
+</html>
+<?php 
+require 'rodape.php';   
+?>
